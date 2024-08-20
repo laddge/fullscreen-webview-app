d96acf3f367cac1178c5c109383d278ff9d379c1
Laddge <dev.laddge@gmail.com>
2024-08-20 20:15:16 +0900

    feat: add webview and prebuild

--- App.tsx
+++ App.tsx
@@ -1,31 +1,36 @@
 import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, TextInput, Button, Text, View } from 'react-native';
+import { StyleSheet, TextInput, Button, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
   const [url, setUrl] = useState('');
   const [loaded, setLoaded] = useState(false);
 
+  if (loaded) {
+    return (
+      <>
+        <WebView
+          style={styles.container}
+          source={{ uri: url }}
+        />
+        <StatusBar hidden />
+      </>
+    );
+  } else {
   return (
     <View style={styles.container}>
-      { loaded ?
-        <>
-          <Text>{url}</Text>
-        </>
-      :
-        <>
-          <TextInput
-            style={styles.input}
-            inputMode="url"
-            onChangeText={setUrl}
-            value={url}
-          />
-          <Button title="Open" onPress={() => setLoaded(true)} />
-        </>
-      }
+      <TextInput
+        style={styles.input}
+        inputMode="url"
+        onChangeText={setUrl}
+        value={url}
+      />
+      <Button title="Open" onPress={() => setLoaded(true)} />
       <StatusBar hidden />
     </View>
   );
+  }
 }
 
 const styles = StyleSheet.create({
